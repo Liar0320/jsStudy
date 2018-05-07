49bebe2292b13635b7404f42263c2ad6b386c275
Liar0320 <100108350@qq.com>
2018-05-07 21:27:06 +0800

    '优化'

--- Vendor/angular-liar/Wframe/app.js
+++ Vendor/angular-liar/Wframe/app.js
@@ -1,20 +1,31 @@
 
 (function(angular){
     'use strict'
-    var app = angular.module('app',['ui.router','oc.lazyLoad']);
-//    app.config(function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
-//         app.register = {
-//             controller: $controllerProvider.register,
-//             directive: $compileProvider.directive,
-//             filter: $filterProvider.register,
-//             factory: $provide.factory,
-//             service: $provide.service
-//         };
-//     });
-
-    app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+   var app = angular.module('app',['ui.router','oc.lazyLoad']);
+   app.config(function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
+        app.register = {
+            controller: $controllerProvider.register,
+            directive: $compileProvider.directive,
+            filter: $filterProvider.register,
+            factory: $provide.factory,
+            service: $provide.service
+        };
+    });
+    app.provider('routerHelp',['$stateProvider',function($stateProvider){
+        var createRouter =function(params){
+            params.forEach(element => {
+                $stateProvider.state(element.name,{url:element.url,controller:element.controller,
+                                    templateUrl:element.templateUrl,resolve:element.resolve})
+            });
+        }
+        this.createRouter = createRouter;
+        this.$get = function(){
+            return createRouter;
+        }
+    }]);
+    app.config(['$stateProvider','$urlRouterProvider','routerHelpProvider','routerConfig',function($stateProvider,$urlRouterProvider,routerHelp,routerConfig){
        $urlRouterProvider.otherwise('login')
-        $stateProvider
+       $stateProvider
             .state('login',{
                 url:'/login',
                 controller:'loginCtrl as vm',
@@ -23,8 +34,13 @@
                     loadCtrl:['$ocLazyLoad',function($ocLazyLoad){
                         return $ocLazyLoad.load('../system/login.js');  
                     }]
-                }}) 
+                }});
+        routerHelp.createRouter(routerConfig);
+    }]);
+    app.provider('ajaxService',['',()=>{
+        
     }])
-
-
+    app.run(['$rootScope',($rootScope)=>{
+        
+    }])
 })(angular);
--- Vendor/angular-liar/Wframe/appConfig.js
+++ Vendor/angular-liar/Wframe/appConfig.js
@@ -1,40 +1,19 @@
 (function(angular){
-    function appConfig(){
-        let  routerConfig = [
-            {
-                name:'goBang',
-                url:'/goBang',
-                controller:'goBangCtrl as vm',
-                templateUrl:'../modules/goBang.html',
-                resolve:{
-                    loadCtrl:['$ocLazyLoad',function($ocLazyLoad){
-                        return $ocLazyLoad.load('../modules/goBang.js');  
-                    }]
-                }
-            },//跳转五子棋页面
-        ];
-        return {
-            routerConfig
-        }
-    }
-
-    angular.module('app').provider('routerHelp',['$stateProvider',function($stateProvider){
-        var createRouter =function(params){
-            if(params === 'app'){
-                if(appConfig !==undefined) params = appConfig().routerConfig;
+    'use strict'
+    let routerConfig = [
+        {
+            name:'goBang',
+            url:'/goBang',
+            controller:'goBangCtrl as vm',
+            templateUrl:'../modules/goBang.html',
+            resolve:{
+                loadCtrl:['$ocLazyLoad',function($ocLazyLoad){
+                    return $ocLazyLoad.load('../modules/goBang.js');  
+                }]
             }
-            params.forEach(element => {
-                $stateProvider.state(element.name,{url:element.url,controller:element.controller,
-                                    templateUrl:element.templateUrl,resolve:element.resolve})
-            });
-        }
-        this.$get = function(){
-            return createRouter;
-        }
-    }]);
+        },//跳转五子棋页面
+    ]
 
-    angular.module('app').config(['routerHelpProvider',(routerHelpProvider)=>{
-        routerHelpProvider.$get()('app');
-    }])
 
+    angular.module('app').constant('routerConfig',routerConfig)
 })(angular);
