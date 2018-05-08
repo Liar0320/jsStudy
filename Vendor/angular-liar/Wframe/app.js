a36599c5f8854fd2065b4d76b73ebc64f965fdc5
Liar0320 <100108350@qq.com>
2018-05-08 20:53:13 +0800

    update

--- Vendor/angular-liar/Wframe/app.js
+++ Vendor/angular-liar/Wframe/app.js
@@ -95,8 +95,6 @@
         $httpProvider.interceptors.push('httpInterceptor');
         $httpProvider.defaults.headers.post["Content-Type"] =
         "application/x-www-form-urlencoded;charset=utf-8";
-         $httpProvider.defaults.headers.put["Content-Type"] =
-        "application/x-www-form-urlencoded;charset=utf-8";
     }])
     app.run(['$rootScope',($rootScope)=>{
         
---
+++ Vendor/codemirror/README.md
@@ -0,0 +1,4 @@
+https://www.awaimai.com/2050.html  CodeMirror在线代码编辑器安装和使用
+https://github.com/codemirror/codemirror   CodeMirror
+http://www.hyjiacan.com/codemirror-event/
+https://www.cnblogs.com/oldphper/p/4065425.html  api
