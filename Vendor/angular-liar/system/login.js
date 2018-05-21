33b9912421a67ce548d331aa6879e5611be2d5e7
Liar0320 <100108350@qq.com>
2018-05-22 07:28:09 +0800

    u

--- Vendor/angular-liar/system/login.js
+++ Vendor/angular-liar/system/login.js
@@ -4,17 +4,18 @@
     loginCtrl.$inject = ['$state','$http','ajaxService'];
     function loginCtrl($state,$http,ajaxService){
         var vm = this;
-       // $state.go('goBang');  
         vm.name='login';
         angular.extend(vm,{
-            url:'login',
-            data:'{"id":0,"password":123456}'
-        })
-        vm.send = ()=>{
-            console.log(vm);
-            ajaxService.post(vm.url,JSON.parse(vm.data),(response, status, headers, config)=>{
-               console.log('success',response, status, headers, config);
-            },(response, status, headers, config)=>{console.log('failed',response, status, headers, config);},true)
+            form:{
+                id:'',
+                password:''
+            },
+            login
+        });
+        function login(){
+             ajaxService.post('/login',vm.form,(data)=>{
+                         console.log('success',data);
+                })
         }
     }
 })(angular);
