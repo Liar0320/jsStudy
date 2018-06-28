4728c4ce60dc91a809f82254f0e13a4fa3befb62
Liar0320 <100108350@qq.com>
2018-06-29 00:44:06 +0800

    es6-es5

--- es6.js
+++ es6.js
@@ -1,3 +1,20 @@
+// 转码结果输出到标准输出
+// babel example.js
+
+// # 转码结果写入一个文件
+// # --out-file 或 -o 参数指定输出文件
+// babel example.js --out-file compiled.js
+// # 或者
+// babel example.js -o compiled.js
+
+// # 整个目录转码
+// # --out-dir 或 -d 参数指定输出目录
+// babel src --out-dir lib
+// # 或者
+// babel src -d lib
+
+// # -s 参数生成source map文件
+// babel src -d lib -s
 // https://www.jianshu.com/p/287e0bb867ae  es6
 
 // https://segmentfault.com/a/1190000008866165        结合microtask和macrotask理解event-loop
@@ -33,6 +50,8 @@ var _promise2 = new Promise(function(resolve,reject){
 const asyncGreet = function (){
     return 909;
 }
+
+let a = a=>a*2
 
 
 
