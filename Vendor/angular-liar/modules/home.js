635e7492138a21478ca116c659063ef84fbfcca3
Liar0320 <100108350@qq.com>
2018-06-30 01:42:31 +0800

    修改了布局

--- Vendor/angular-liar/modules/home.js
+++ Vendor/angular-liar/modules/home.js
@@ -1,66 +1,92 @@
-(function(angular){
-    var home = angular.module('app.home',[]);
-    home.controller('homeCtrl',homeCtrl)
-    homeCtrl.$inject = [];
-    function homeCtrl() {
-        var vm = this;
-        vm.titleSets = ['首页','全部课程','Unity','虚幻','关于我们'];
-        console.log(this);
-    }
+(function(angular) {
+  var home = angular.module("app.home", []);
+  home.controller("homeCtrl", homeCtrl);
+  homeCtrl.$inject = ["ajaxService"];
+  function homeCtrl(ajaxService) {
+    var vm = this;
+    // vm.titleSets = ['首页','全部课程','Unity','虚幻','关于我们'];
+    //   console.log(this);
+    //   ajaxService.get('/menuList','',function(res){
+    //       console.log(res);
+    //       vm.titleSets = res.data.record;
+    //   })
+  }
 
-
-    home.directive('decBroadCast',decBroadCast)
-    function decBroadCast() {
-        return{
-            restrict:'A',
-            link:link,
+  home.directive("decBroadCast", decBroadCast);
+  function decBroadCast() {
+    return {
+      restrict: "A",
+      link: link
+    };
+    function link(scope, ele, attrs) {
+      let animation = attrs.animation;
+      let len = ele[0].getElementsByTagName("img").length;
+      let width = parseInt(
+        $.getStyle(ele[0].getElementsByTagName("img")[0], "width")
+      );
+      $.setStyle(ele[0], "width", `${len * width}px`);
+      $.setStyle(ele[0], "left", `${width}px`);
+      let index = 1;
+      $.setStyle(ele[0], "transform", `translate3d(-${index * width}px,0,0)`);
+      setInterval(() => {
+        index++;
+        if (!$.hasClass(ele[0], animation)) $.addClass(ele[0], animation);
+        $.setStyle(ele[0], "transform", `translate3d(-${index * width}px,0,0)`);
+        if (index === len) {
+          setTimeout(() => {
+            $.removeClass(ele[0], animation);
+            index = 1;
+            $.setStyle(
+              ele[0],
+              "transform",
+              `translate3d(-${index * width}px,0,0)`
+            );
+          }, 1000);
         }
-        function link(scope,ele,attrs) {
-            let animation = attrs.animation;
-            let len = ele[0].querySelectorAll('img').length;
-            let width = parseInt($.getStyle(ele[0].querySelectorAll('img')[0],'width'));
-            $.setStyle(ele[0],'width',`${len*width}px`);
-            $.setStyle(ele[0],'left',`${width}px`);
-            let index = 1; 
-            $.setStyle(ele[0],'transform',`translate3d(-${index*width}px,0,0)`);
-            setInterval(()=>{
-                index++;
-                if(!$.hasClass(ele[0],animation))$.addClass(ele[0],animation)
-                $.setStyle(ele[0],'transform',`translate3d(-${index*width}px,0,0)`);
-                if(index===len){
-                    setTimeout(()=>{
-                    $.removeClass(ele[0],animation);
-                    index = 1;
-                    $.setStyle(ele[0],'transform',`translate3d(-${index*width}px,0,0)`);
-                    },1000)
-                }
-            },10000)
+        setCircleActive(index);
+      }, 10000);
 
-            ele[0].parentNode.appendChild(getCricleHtml(len));
-
-
-            function getCricleHtml(len){
-                let div = document.createElement('div');
-                div.className = 'pa tc pageControl';
-                for (let i = 0; i< len - 1 ; i++) {
-                   let circle = document.createElement('div');
-                   circle.className = 'pageControl-circle mr15';
-                   circle.addEventListener('click',()=>{
-                        index = i + 1;
-                        if(!$.hasClass(ele[0],animation))$.addClass(ele[0],animation)
-                        $.setStyle(ele[0],'transform',`translate3d(-${index*width}px,0,0)`);
-                   })
-                   div.appendChild(circle);
-                   circle = null;
-                }
-                return div;
-            }
+      //创建节点
+      function getCircleHtml(len) {
+        let div = document.createElement("div");
+        div.className = "pa tc pageControl";
+        for (let i = 0; i < len - 1; i++) {
+          let circle = document.createElement("div");
+          circle.className = "pageControl-circle mr15";
+          circle.addEventListener("click", () => {
+            index = i + 1;
+            if (!$.hasClass(ele[0], animation)) $.addClass(ele[0], animation);
+            $.setStyle(
+              ele[0],
+              "transform",
+              `translate3d(-${index * width}px,0,0)`
+            );
+            setCircleActive(index);
+          });
+          div.appendChild(circle);
+          circle = null;
         }
-    }
+        $.setStyle(div.$children()[0], "border-color", "#dedede");
+        return div;
+      }
 
+      //创建所有点集合
+      var circle = getCircleHtml(len);
+      ele[0].parentNode.appendChild(circle);
+      //设置当前选中的点
+      function setCircleActive(index) {
+        index === len ? (index = 0) : (index -= 1);
+        for (var i = 0; i < len - 1; i++) {
+          index === i
+            ? $.setStyle(circle.$children()[i], "border-color", "#dedede")
+            : $.setStyle(circle.$children()[i], "border-color", "inherit");
+        }
+      }
+    }
+  }
 
   //  home
-})(angular)
+})(angular);
 
 //decBroadCast
 /*
