e77e0b133fbc850ec89d01f4e11a869607e7a9f9
Liar0320 <100108350@qq.com>
2018-07-09 23:10:22 +0800

    修改了懒加载

--- Vendor/angular-liar/Wframe/appConfig.js
+++ Vendor/angular-liar/Wframe/appConfig.js
@@ -1,32 +1,38 @@
 (function(angular){
-    'use strict'
-    let routerConfig = [
+    'use strict';
+    angular.module('app').config(['$ocLazyLoadProvider','APP_REQUIRES',function($ocLazyLoadProvider,APP_REQUIRES){
+        $ocLazyLoadProvider.config({
+            modules:APP_REQUIRES.modules,
+            debug:false,
+            events:true
+        });
+    }])
+    .constant('routerConfig', [
         {
             name:'login',
             url:'/login',
             controller:'loginCtrl as vm',
             templateUrl:'../system/login.html',
-            resolve:{
-                loadCtrl:['$ocLazyLoad',function($ocLazyLoad){
-                    return $ocLazyLoad.load('../system/login.js');  
-                }]
-            }
+            resolve:'../system/login.js'
         },
-  
         {
             name:'app.goBang',
             url:'/goBang',
             controller:'goBangCtrl as vm',
             templateUrl:'../modules/goBang.html',
-            resolve:{
-                loadCtrl:['$ocLazyLoad',function($ocLazyLoad){
-                    return $ocLazyLoad.load('../modules/goBang.js');  
-                }]
-            }
+            resolve:'../modules/goBang.js'
         },//跳转五子棋页面
     ]
-
-
-    angular.module('app').constant('routerConfig',routerConfig);
+)
+    .constant('APP_REQUIRES',{
+        modules:[{
+            name:'encrypt',
+            files:['../node_modules/md5/md5.js']
+        }
+        ]
+    })
+    .constant('APP_LOADMOD',{
+        'login':['encrypt']
+    });
 })(angular);
 
