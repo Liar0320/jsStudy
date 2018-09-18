00c0de8f93548e1070bb8a1094a294b792fd0ec7
Liar0320 <100108350@qq.com>
2018-09-19 00:28:43 +0800

    实现 atoi，将字符串转为整数。

--- leetcode.js
+++ leetcode.js
@@ -387,7 +387,30 @@ var longestCommonPrefix = function(strs) {
 };
 
 
+//实现 atoi，将字符串转为整数。
+var myAtoi = function(str) {
+    var s = parseInt(str)||0
+    return s < -2147483648 ? -2147483648 : s>2147483647 ? 2147483647:s;
+};
 
+var myAtoi = function(str) {
+    var s = (str.match(/^\s*([-+]?)(\d+)/)||[])[0] *1;
+    if(!s)return 0;
+    return s < -2147483648 ? -2147483648 : s>2147483647 ? 2147483647:s;
+};
+
+var myAtoi = function(str) {
+    var s = (str.replace(/^\s+/,'').match(/^[-+]?\d+/)||[])[0] *1 || 0;
+    return s < -2147483648 ? -2147483648 : s>2147483647 ? 2147483647:s;
+};
+
+var myAtoi = function(str) {
+    var s = (str.replace(/^\s+/,'').match(/^[-+]?\d+/)||[])[0] *1;
+    if(!s)return 0;
+    let min = Math.pow(-2,31) ;
+    let max = Math.pow(2,31) -1;
+    return s < min ? min : s>max ? max:s;
+};
 
 
 
