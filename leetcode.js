a6b08a61b2cc41ba5c1f460cbfff5584147f4d7c
Liar0320 <100108350@qq.com>
2018-11-24 00:36:17 +0800

    添加对称二叉树

--- leetcode.js
+++ leetcode.js
@@ -1,8 +1,8 @@
 /*
  * @Author: liar 
  * @Date: 2018-09-21 00:07:50 
- * @Last Modified by: lich
- * @Last Modified time: 2018-09-28 10:26:55
+ * @Last Modified by: liar
+ * @Last Modified time: 2018-11-24 00:35:37
  */
 //1 2 3 4 5 6 7        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~神奇    代码其实是逻辑思维的展示，逻辑的好坏在一定程度上决定了代码的好坏。
 (function(){
@@ -533,30 +533,30 @@ var removeNthFromEnd = function(head, n) {
  * @param {*} head 
  */
 var reverseList = function(head) {
-    if(!head || head.next === null) return head;
-    var rn = new ListNode();
-    var q1 = rn;
-    var q2 = head.next.next;
-    var q3 = head;
+	if(!head || head.next === null) return head;
+	var rn = new ListNode();
+	var q1 = rn;
+	var q2 = head.next.next;
+	var q3 = head;
     
-    while(true){
-        while(q2 !== null){
-            q2 = q2.next;
-            q3 = q3.next;
-        }
-        if(q1.val === undefined){
-             q1.val = q3.next.val;
-        }else{
-            q1.next = q3.next;
-            q1 = q1.next;
-        }
-        q3.next = null;
-        if(head.next === null) break;
-        q2 = head.next.next;
-        q3 = head;
-    }
-    q1.next = head;
-    return rn;
+	while(true){
+		while(q2 !== null){
+			q2 = q2.next;
+			q3 = q3.next;
+		}
+		if(q1.val === undefined){
+			q1.val = q3.next.val;
+		}else{
+			q1.next = q3.next;
+			q1 = q1.next;
+		}
+		q3.next = null;
+		if(head.next === null) break;
+		q2 = head.next.next;
+		q3 = head;
+	}
+	q1.next = head;
+	return rn;
 };
 
 var reverseList = function (head) {
@@ -570,57 +570,57 @@ var reverseList = function (head) {
 		node = tempNext;
 	}
 	return prev;
-}
+};
 
 var reverseList = function (head) {
-    if(head === null || head.next === null){
-        return head;
-    }else{
-        var result = reverseList(head.next);
-        var nextNode = head.next;
-        nextNode.next = head;
-        head.next = null;
-    }
-    return result;
-}
+	if(head === null || head.next === null){
+		return head;
+	}else{
+		var result = reverseList(head.next);
+		var nextNode = head.next;
+		nextNode.next = head;
+		head.next = null;
+	}
+	return result;
+};
 
 //将两个有序链表 合成一个
 var mergeTwoLists = function(l1, l2) {
-    var q1 = l1;
+	var q1 = l1;
 	var q2 = l2;
 	var isRun ;
-    var head;
+	var head;
 	var node ;
 	while (true) {
 		if(q1!==null && q2 !==null){
 			isRun =  q1.val < q2.val? 1:2;
 			if(node === undefined){
 				node = isRun === 1? q1:q2;
-                head = node;
-                isRun === 1? (q1 = q1.next):(q2 = q2.next);
+				head = node;
+				isRun === 1? (q1 = q1.next):(q2 = q2.next);
 			}else{
 				node.next = isRun === 1? q1:q2;
-                isRun === 1? (q1 = q1.next):(q2 = q2.next);
-                node = node.next;
+				isRun === 1? (q1 = q1.next):(q2 = q2.next);
+				node = node.next;
 			}
 		}else if(q1 === null && q2 ===null){
 			return head || null;
 		}else if(q1 === null){
-            if(node){
-               node.next = q2; 
-            }else{
-                node = q2;
-                head = node;
-            }
+			if(node){
+				node.next = q2; 
+			}else{
+				node = q2;
+				head = node;
+			}
            
-		   return head
+		   return head;
 		}else{
-             if(node){
-               node.next = q1; 
-            }else{
-                node = q1;
-                head = node;
-            }
+			if(node){
+				node.next = q1; 
+			}else{
+				node = q1;
+				head = node;
+			}
 			return head;
 		}
 
@@ -630,13 +630,13 @@ var mergeTwoLists = function(l1, l2) {
 //生成一个反转的链表 和一个正向链表
 function reseverNode (head){
 	if(head === null || head.next === null ){
-		var result = head? new ListNode(head.val) : null
+		var result = head? new ListNode(head.val) : null;
 		return [result,result,result];
 	}else{
 		var result = reseverNode(head.next);
 		var node = new ListNode(head.val);
 		result[0].next = node;
-		return [node,result[1],head]
+		return [node,result[1],head];
 	}
 }
 
@@ -652,35 +652,85 @@ function reseverNode (head){
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    var q1 = head;
-    var q2;
+	var q1 = head;
+	var q2;
 
-    var preNode = null;
-    var tempNode = null ;
-    while(head !== null){
-        if(tempNode){
-            tempNode = new ListNode(head.val);
-            tempNode.next = preNode;
-            preNode = tempNode;
-        }else{
-            tempNode = new ListNode(head.val);
-            preNode = tempNode;
-        }
-        head = head.next;
-    }
-    q2 = tempNode;
+	var preNode = null;
+	var tempNode = null ;
+	while(head !== null){
+		if(tempNode){
+			tempNode = new ListNode(head.val);
+			tempNode.next = preNode;
+			preNode = tempNode;
+		}else{
+			tempNode = new ListNode(head.val);
+			preNode = tempNode;
+		}
+		head = head.next;
+	}
+	q2 = tempNode;
   
     
-    if(q1 === q2) return true;
-    while((q1 !== null || q2 !== null)&&(q1.val === q2.val)){
-         q1 = q1.next;
-         q2 = q2.next;
-   }
-    if(q1 === null && q2 ===null){
-        return true;
-    }else{
-        return false;
-    }
+	if(q1 === q2) return true;
+	while((q1 !== null || q2 !== null)&&(q1.val === q2.val)){
+		q1 = q1.next;
+		q2 = q2.next;
+	}
+	if(q1 === null && q2 ===null){
+		return true;
+	}else{
+		return false;
+	}
   
 
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+	var level = 1;
+	var bol = true;
+	while(root){
+		var result = bstToArrayLevel(root,level);
+		if(result.every(item=> item === null)) break;
+		if(!compareResever(result)){
+			bol = false;
+			break;
+		}
+		level++;
+	}
+	return bol;
+};
+
+//将二叉树 转化为数组形式
+function bstToArrayLevel(treeNode,level){
+	var result = [];
+	if(level === 0){
+		return [treeNode === null ? null : treeNode.val];
+	}
+	if(treeNode){
+		result = result.concat(bstToArrayLevel(treeNode.left,level-1));
+    	result = result.concat(bstToArrayLevel(treeNode.right,level-1));
+	}
+
+	return result;
+}
+
+//判断一个数组是否为回文
+function compareResever(rows){
+	var len = rows.length;
+	var start = -1;
+	do{
+		if(rows[++start] !== rows[--len]) break;
+	}while(start < len);
+	return  (--start === len);
+}
