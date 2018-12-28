d7f173d6635df953b7b6147d6af2bfa3956fca0b
lich <100108350@qq.com>
2018-12-28 17:36:08 +0800

    leetcode联系

--- leetcode.js
+++ leetcode.js
@@ -2,7 +2,7 @@
  * @Author: liar 
  * @Date: 2018-09-21 00:07:50 
  * @Last Modified by: liar
- * @Last Modified time: 2018-11-24 00:38:12
+ * @Last Modified time: 2018-12-28 13:28:56
  */
 //1 2 3 4 5 6 7        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~神奇    代码其实是逻辑思维的展示，逻辑的好坏在一定程度上决定了代码的好坏。
 (function(){
@@ -734,3 +734,33 @@ function compareResever(rows){
 	}while(start < len);
 	return  (--start === len);
 }
+
+
+
+/**
+ * 88. 合并两个有序数组
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ * 因为两个数组都是有序的
+所以
+	A数组为参照对象
+	将B数组的元素 依次插入 A数组
+	直到B数组的长度为零
+	
+	找到一个比temp大的元素 插入其前面
+ */
+var merge = function(nums1, m, nums2, n) {
+    var temp = 0,i = 0;
+    nums1.length = m;
+    nums2.length = n;
+    while(nums2.length !== 0){
+       temp = nums2.shift();
+       while(nums1[i] < temp){
+           i++
+       }
+        nums1.splice(i,0,temp);
+    }
+};
